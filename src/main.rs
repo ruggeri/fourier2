cca3e813dbd096b6acba6d6f3735c44c3a1d79d6
Ned Ruggeri <ruggeri@self-loop.com>
2018-06-07 14:15:18 -0700

    do note detection on more complex note progression

--- src/main.rs
+++ src/main.rs
@@ -7,39 +7,41 @@ extern crate fourier2;
 
 use byteorder::{LittleEndian, WriteBytesExt};
 use std::fs::File;
-use fourier2::{Chord, ftransform, Note, Pitch, scale, util};
+use fourier2::{Note, scale};
 
-fn play(f: &mut File, chord: &Chord, duration: f64) {
+fn play<F>(file: &mut File, f: &F, duration: f64)
+  where F: Fn(f64) -> f64 {
   let mut t = 0.0;
   while t < duration {
-    let val: f64 = chord.val(t);
-    f.write_i16::<LittleEndian>(fourier2::util::f64_to_i16(val)).unwrap();
+    let val: f64 = f(t);
+    file.write_i16::<LittleEndian>(fourier2::util::f64_to_i16(val)).unwrap();
 
     t += 1_f64 / fourier2::constants::SAMPLE_RATE;
   }
 }
 
 fn main() {
-  let pitches: Vec<Pitch> = vec![
-    "A.3".parse().unwrap(),
-    "C.3".parse().unwrap(),
-    "E.3".parse().unwrap(),
-    "A.4".parse().unwrap(),
+  let notes: Vec<Note> = vec![
+    Note::new("A.3".parse().unwrap(), 0.0, 4.0, 0.1),
+    Note::new("C.3".parse().unwrap(), 1.0, 4.0, 0.1),
+    Note::new("E.3".parse().unwrap(), 2.0, 4.0, 0.1),
+    Note::new("A.4".parse().unwrap(), 3.0, 4.0, 0.1),
   ];
 
-  let chord = Chord::new(pitches, 0.25, 3.5, 0.1);
+  let f = |t| {
+    Note::total_val(notes.iter(), t)
+  };
 
   println!("Writing PCM file!");
   let mut file = File::create("./samples.pcm").unwrap();
-  play(&mut file, &chord, 4.0);
+  play(&mut file, &f, 4.0);
 
   println!("Searching for freqs!");
-  for p in scale::piano_pitches() {
-    let hz = p.freq();
-    let coeffs = ftransform(hz as f64, |t| chord.val(t), 2.0);
-    let amplitude = util::amplitude(coeffs);
-    if amplitude > 0.075 {
-      println!("{:?}: {:?}", p, amplitude);
-    }
+
+  let mut t = 0.5;
+  while t < 4.0 {
+    println!("{:?}", scale::scan(&f, t));
+
+    t += 1.0;
   }
 }
