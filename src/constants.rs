9455dc3230889f13be40bc691ff67d7911afe708
Ned Ruggeri <ruggeri@self-loop.com>
2018-06-07 14:14:29 -0700

    add code to scan for piano notes

--- src/constants.rs
+++ src/constants.rs
@@ -1,1 +1,2 @@
 pub const SAMPLE_RATE: f64 = 44_100.0;
+pub const SCAN_THRESHOLD: f64 = 0.075_f64;
--- src/scale.rs
+++ src/scale.rs
@@ -1,4 +1,7 @@
+use constants;
 use pitch::Pitch;
+use transform::ftransform;
+use util;
 
 pub fn scale_for_octave(octave: i32) -> Vec<Pitch> {
   vec![
@@ -24,4 +27,20 @@ pub fn piano_pitches() -> Vec<Pitch> {
   }
 
   pitches
+}
+
+pub fn scan<F>(f: F, t: f64) -> Vec<Pitch>
+  where F: Fn(f64) -> f64 {
+  let mut detected_pitches = Vec::<Pitch>::new();
+
+  for p in piano_pitches() {
+    let hz = p.freq();
+    let coeffs = ftransform(hz as f64, |t| f(t), t);
+    let amplitude = util::amplitude(coeffs);
+    if amplitude > constants::SCAN_THRESHOLD {
+      detected_pitches.push(p);
+    }
+  }
+
+  detected_pitches
 }
