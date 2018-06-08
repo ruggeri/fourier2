8cb4a55026803729e76f5fb5655ee297ce4a4e63
Ned Ruggeri <ruggeri@self-loop.com>
2018-06-08 13:27:05 -0700

    decrease threshold for reported notes

--- src/constants.rs
+++ src/constants.rs
@@ -3,5 +3,5 @@ pub const FOURIER_SAMPLE_RATE_PER_SEC: f64 = SAMPLE_RATE / 1.0_f64;
 pub const FOURIER_WINDOW_WIDTH: f64 = 0.1_f64;
 pub const NOISE_LEVEL: f64 = 0.1_f64;
 pub const SAMPLE_RATE: f64 = 44_100.0;
-pub const SCAN_THRESHOLD: f64 = 0.0075_f64;
+pub const SCAN_THRESHOLD: f64 = 0.002_f64;
 pub const SCAN_TIME_RESOLUTION: f64 = 0.1_f64;
