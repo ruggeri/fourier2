04c03f2a5d4f77755e0caea19c0d83a4bfe68eb5
Ned Ruggeri <ruggeri@self-loop.com>
2018-06-09 22:25:41 -0700

    refactor hz scanner module

---
+++ src/lib/hz_scanner/mod.rs
@@ -0,0 +1,5 @@
+mod opts;
+mod scanner;
+
+pub use self::opts::{HzScannerOpts, HzScannerOptsBuilder};
+pub use self::scanner::{DetectedHz, HzScanner};
---
+++ src/lib/hz_scanner/opts.rs
@@ -0,0 +1,24 @@
+use constants::*;
+use transform::{FourierTransformOpts, FourierTransformOptsBuilder};
+
+#[derive(Builder, Clone, Copy)]
+pub struct HzScannerOpts {
+  pub start_hz: f64,
+  pub end_hz: f64,
+  #[builder(default = "SCAN_TIME_RESOLUTION")]
+  pub scan_time_resolution: f64,
+  #[builder(default = "FOURIER_WINDOW_WIDTH")]
+  pub window_width: f64,
+  #[builder(default = "FOURIER_SAMPLE_RATE_PER_SEC")]
+  pub sample_rate_per_sec: f64,
+}
+
+impl From<HzScannerOpts> for FourierTransformOpts {
+  fn from(opts: HzScannerOpts) -> FourierTransformOpts {
+    FourierTransformOptsBuilder
+      ::default()
+      .window_width(opts.window_width)
+      .sample_rate_per_sec(opts.sample_rate_per_sec)
+      .build().unwrap()
+  }
+}
